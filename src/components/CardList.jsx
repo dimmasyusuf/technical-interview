17e27706f59127d091d08f3009e3986b8f5eb881
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-03-13 16:03:17 +0700

    feat: dispatch getAllProducts

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,8 +1,15 @@
 import { SimpleGrid, Text, Card, CardBody } from '@chakra-ui/react';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllProducts } from '../app/features/productSlice';
 
 export default function CardList() {
   const { products } = useSelector((state) => state.products);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllProducts());
+  }, [dispatch]);
 
   console.log('products:', products);
   return (
