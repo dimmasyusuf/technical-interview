a5d700ba295473f4e16a092703a02a4d77f4651e
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-03-13 14:53:47 +0700

    chore: add react router dom for navigation

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Provider as ReactProvider } from 'react-redux';
 import { store } from '../app/store';
 import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
 
 export default function Provider({ children }: { children: React.ReactNode }) {
   return (
-    <ReactProvider store={store}>
-      <ChakraProvider>{children}</ChakraProvider>
-    </ReactProvider>
+    <BrowserRouter>
+      <ReactProvider store={store}>
+        <ChakraProvider>{children}</ChakraProvider>
+      </ReactProvider>
+    </BrowserRouter>
   );
 }
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,10 @@
+import { Button, Flex } from '@chakra-ui/react';
+import React from 'react';
+
+export default function Homepage() {
+  return (
+    <Flex>
+      <Button>Click me</Button>
+    </Flex>
+  );
+}
