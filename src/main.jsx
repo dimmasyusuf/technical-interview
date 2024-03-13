d28b0f997c9645cf341e0d4e65961b0702183b78
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-03-13 14:48:57 +0700

    fix: change provider type into tsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import Provider from './components/Provider.js';
+import Provider from './components/Provider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
